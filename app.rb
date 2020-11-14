8a92dca4f4437fef61c291bb2ced58c56c7cae3f
John Carter <johncarter352@gmail.com>
2020-11-14 23:11:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,16 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/name' do 
-    "My name is "
+    "My name is __"
+  end
+  
+  get '/hometown' do
+    "My hometown is __"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is __"
+  end
   
   
   
